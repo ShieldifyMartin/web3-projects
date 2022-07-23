acfe20e9b874fab0f4afc69599175d15a4d4fcb0
unknown <martin.petrov033@gmail.com>
2022-07-23 14:38:44 +0300

    Add give-right-to-vote functionality

--- solidity-tutorials/Voting.sol
+++ solidity-tutorials/Voting.sol
@@ -28,4 +28,14 @@ contract Ballot {
             proposals.push(Proposal({name: proposalNames[i], voteCount: 0}));
         }
     }
+
+    function giveRightToVote(address voter) external {
+        require(
+            msg.sender == chairperson,
+            "Only chairperson can give right to vote."
+        );
+        require(!voters[voter].voted, "The voter already voted.");
+        require(voters[voter].weight == 0);
+        voters[voter].weight = 1;
+    }
 }
