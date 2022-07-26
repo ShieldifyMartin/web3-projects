bcd445206bdb56865e578c91800156b90021b68d
unknown <martin.petrov033@gmail.com>
2022-07-26 19:56:52 +0300

    Add voting functionality

--- solidity-tutorials/Voting.sol
+++ solidity-tutorials/Voting.sol
@@ -62,4 +62,15 @@ contract Ballot {
             delegate_.weight += sender.weight;
         }
     }
+
+    function vote(uint256 proposal) external {
+        Voter storage sender = voters[msg.sender];
+        require(sender.weight != 0, "Has no right to vote");
+        require(!sender.voted, "Already voted.");
+
+        sender.voted = true;
+        sender.vote = proposal;
+
+        proposals[proposal].voteCount += sender.weight;
+    }
 }
