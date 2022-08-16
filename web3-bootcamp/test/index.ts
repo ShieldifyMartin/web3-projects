a64752ed03e6c1389854b40d60db0a8d284be957
unknown <martin.petrov033@gmail.com>
2022-08-16 18:02:19 +0300

    Add tests and modify deploy script

--- web3-bootcamp/test/index.ts
+++ web3-bootcamp/test/index.ts
@@ -1,19 +1,42 @@
-import { expect } from "chai";
-import { ethers } from "hardhat";
+const { ethers } = require("hardhat");
+const { assert } = require("chai");
+import { SimpleStorage, SimpleStorage__factory } from "../typechain-types";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("SimpleStorage", function () {
+  let simpleStorageFactory: SimpleStorage__factory;
+  let simpleStorage: SimpleStorage;
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  beforeEach(async function () {
+    simpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+    simpleStorage = await simpleStorageFactory.deploy();
+  });
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+  it("Should start with a favorite number of 0", async function () {
+    const currentValue = await simpleStorage.retrieve();
+    const expectedValue = "0";
+    assert.equal(currentValue.toString(), expectedValue);
+  });
+  it("Should update when we call store", async function () {
+    const expectedValue = "7";
+    const transactionResponse = await simpleStorage.store(expectedValue);
+    await transactionResponse.wait(1);
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    const currentValue = await simpleStorage.retrieve();
+    assert.equal(currentValue.toString(), expectedValue);
+  });
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+  // Extra - this is not in the video
+  it("Should work correctly with the people struct and array", async function () {
+    const expectedPersonName = "Patrick";
+    const expectedFavoriteNumber = "16";
+    const transactionResponse = await simpleStorage.addPerson(
+      expectedPersonName,
+      expectedFavoriteNumber
+    );
+    await transactionResponse.wait(1);
+    const { favoriteNumber, name } = await simpleStorage.people(0);
+
+    assert.equal(name, expectedPersonName);
+    assert.equal(favoriteNumber, expectedFavoriteNumber);
   });
 });
