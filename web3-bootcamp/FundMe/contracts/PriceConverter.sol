d9bc781f1d36b28587bad882456d5bc800360051
unknown <martin.petrov033@gmail.com>
2022-08-24 19:10:09 +0300

    Add PriceConverter smart contract

---
+++ web3-bootcamp/FundMe/contracts/PriceConverter.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+library PriceConverter {
+    /** @notice Gets ether price
+     *  @param priceFeed price feed
+     *  @return ethPrice price of ether
+     */
+    function getPrice(AggregatorV3Interface priceFeed)
+        internal
+        view
+        returns (uint256 ethPrice)
+    {
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        ethPrice = uint256(answer * 1e10);
+    }
+
+    /** @notice Gets the amount that an address has funded
+     *  @param ethAmount amount of ether
+     *  @param priceFeed price feed
+     *  @return ethAmountInUsd usd amount
+     */
+    function getConversionRate(
+        uint256 ethAmount,
+        AggregatorV3Interface priceFeed
+    ) internal view returns (uint256 ethAmountInUsd) {
+        uint256 ethPrice = getPrice(priceFeed);
+        ethAmountInUsd = (ethPrice * ethAmount) / 10**18;
+    }
+}
