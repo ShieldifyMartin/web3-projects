9e56b3fe6a3292478860c86d01820205e762d4e5
unknown <martin.petrov033@gmail.com>
2022-09-21 14:41:29 +0300

    Add network configuration and deployment

--- web3-bootcamp/Raffle/hardhat.config.js
+++ web3-bootcamp/Raffle/hardhat.config.js
@@ -1,11 +1,36 @@
-require("@nomiclabs/hardhat-waffle")
-require("@nomiclabs/hardhat-etherscan")
-require("hardhat-deploy")
-require("solidity-coverage")
-require("hardhat-gas-reporter")
-require("hardhat-contract-sizer")
-require("dotenv").config()
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+require("solidity-coverage");
+require("hardhat-gas-reporter");
+require("hardhat-contract-sizer");
+require("dotenv").config();
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+        },
+    },
     solidity: "0.8.16",
-}
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+};
---
+++ web3-bootcamp/Raffle/helpers/hardhat-config.js
@@ -0,0 +1,13 @@
+const networkConfig = {
+    5: {
+        name: "goerli",
+        vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
+    },
+};
+
+const developmentChains = ["hardhat", "localhost"];
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+};
