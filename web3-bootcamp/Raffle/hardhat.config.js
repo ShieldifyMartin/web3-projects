79c9ab085f4cb0c6403a8e2bac6b8348fa7604a2
unknown <martin.petrov033@gmail.com>
2022-09-30 15:23:32 +0300

    Add project style changes

--- web3-bootcamp/Raffle/hardhat.config.js
+++ web3-bootcamp/Raffle/hardhat.config.js
@@ -8,7 +8,6 @@ require("dotenv").config();
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "";
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 module.exports = {
