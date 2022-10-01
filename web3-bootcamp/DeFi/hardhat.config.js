f8a0de2372af90e5d683e21f428c0ac3eb412628
unknown <martin.petrov033@gmail.com>
2022-10-01 15:34:26 +0300

    Add project base

---
+++ web3-bootcamp/DeFi/hardhat.config.js
@@ -0,0 +1,66 @@
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+require("solidity-coverage");
+require("hardhat-deploy");
+
+const MAINNET_RPC_URL = process.env.ALCHEMY_MAINNET_RPC_URL || "";
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      forking: {
+        url: MAINNET_RPC_URL,
+      },
+    },
+    localhost: {
+      chainId: 31337,
+    },
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+      blockConfirmations: 6,
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17",
+      },
+      {
+        version: "0.6.12",
+      },
+      {
+        version: "0.6.6",
+      },
+      {
+        version: "0.6.0",
+      },
+      {
+        version: "0.4.19",
+      },
+    ],
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+  },
+};
