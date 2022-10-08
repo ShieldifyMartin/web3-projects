21df6246882483bafb55ef692ca75c6e0e807901
unknown <martin.petrov033@gmail.com>
2022-10-08 15:53:10 +0300

    Add borrowing dai functionality

--- web3-bootcamp/DeFi/scripts/aaveBorrow.js
+++ web3-bootcamp/DeFi/scripts/aaveBorrow.js
@@ -16,6 +16,55 @@ const main = async () => {
     gasLimit: 800000,
   });
   console.log("Deposited!");
+
+  // Borrow
+  const { availableBorrowsETH, totalDebtETH } = await getBorrowUserData(
+    lendingPool,
+    deployer
+  );
+  const daiPrice = await getDaiPrice();
+  const daiTokenAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+  const amountDaiToBorrow =
+    availableBorrowsETH.toString() * 0.95 * (1 / daiPrice.toNumber());
+  console.log(`You can borrow ${amountDaiToBorrow} DAI.`);
+  const amountDaiToBorrowWei = ethers.utils.parseEther(
+    amountDaiToBorrow.toString()
+  );
+  await borrowDai(daiTokenAddress, lendingPool, amountDaiToBorrowWei, deployer);
+};
+
+const getDaiPrice = async () => {
+  const daiEthPriceFeed = await ethers.getContractAt(
+    "AggregatorV3Interface",
+    "0x773616E4d11A78F511299002da57A0a94577F1f4"
+  );
+  const price = (await daiEthPriceFeed.latestRoundData())[1];
+  console.log(`The DAI/ETH price is ${price.toString()}`);
+  return price;
+};
+
+const getBorrowUserData = async (lendingPool, account) => {
+  const {
+    totalCollateralETH,
+    totalDebtETH,
+    availableBorrowsETH,
+  } = await lendingPool.getUserAccountData(account);
+  console.log("-----------");
+  console.log(
+    `You have ${ethers.utils.formatEther(
+      totalCollateralETH
+    )} worth of ETH deposited.`
+  );
+  console.log(
+    `You have ${ethers.utils.formatEther(totalDebtETH)} worth of ETH borrowed`
+  );
+  console.log(
+    `You can borrow ${ethers.utils.formatEther(
+      availableBorrowsETH
+    )} worth of ETH.`
+  );
+  console.log("-----------");
+  return { availableBorrowsETH, totalDebtETH };
 };
 
 const getLendingPool = async (account) => {
@@ -39,6 +88,25 @@ const approveERC20 = async (erc20Address, spenderAddress, amount, signer) => {
   await txResponse.wait(1);
   console.log("Approved!");
   console.log("-----------");
+};
+
+const borrowDai = async (
+  daiAddress,
+  lendingPool,
+  amountDaiToBorrowWei,
+  account
+) => {
+  const borrowTx = await lendingPool.borrow(
+    daiAddress,
+    amountDaiToBorrowWei,
+    1,
+    0,
+    account
+  );
+  await borrowTx.wait(1);
+  console.log(
+    `You have borrowed ${ethers.utils.formatEther(amountDaiToBorrowWei)} ETH!`
+  );
 };
 
 main()
