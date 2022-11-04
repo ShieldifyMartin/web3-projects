07780b091202b37f9bb7ac7fd3f2498b289b1b28
unknown <martin.petrov033@gmail.com>
2022-11-04 22:14:57 +0200

    Modify minting logic and add getter functions

--- web3-bootcamp/NFT/contracts/RandomIpfsNft.sol
+++ web3-bootcamp/NFT/contracts/RandomIpfsNft.sol
@@ -11,31 +11,46 @@ error RandomIpfsNft__RangeOutOfBounds();
 error RandomIpfsNft__NeedMoreETHSent();
 error RandomIpfsNft__TransferFailed();
 
-contract RandomIpfsNft is VRFConsumerBaseV2, ERC721URIStorage, Ownable {
+contract RandomIpfsNft is
+    VRFConsumerBaseV2,
+    ERC721URIStorage,
+    Ownable
+{
     enum Breed {
         PUG,
         SHIBA_INU,
         ST_BERNARD
     }
 
-    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    VRFCoordinatorV2Interface
+        private immutable i_vrfCoordinator;
     uint64 private immutable i_subscriptionId;
     bytes32 private immutable i_gasLane;
     uint32 private immutable i_callbackGasLimit;
-    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint16
+        private constant REQUEST_CONFIRMATIONS =
+        3;
     uint32 private constant NUM_WORDS = 1;
     uint256 internal immutable i_mintFee;
 
     // NFT Variables
     uint256 public s_tokenCounter;
-    uint256 internal constant MAX_CHANCE_VALUE = 100;
+    uint256 internal constant MAX_CHANCE_VALUE =
+        100;
     string[] internal i_nftTokenUries;
 
-    mapping(uint256 => address) public s_requestIdSenders;
+    mapping(uint256 => address)
+        public s_requestIdSenders;
 
     // Events
-    event NftRequested(uint256 indexed requestId, address requester);
-    event NftMinted(Breed breed, address indexed minter);
+    event NftRequested(
+        uint256 indexed requestId,
+        address requester
+    );
+    event NftMinted(
+        Breed breed,
+        address indexed minter
+    );
 
     constructor(
         address vrfCoordinatorV2,
@@ -44,8 +59,13 @@ contract RandomIpfsNft is VRFConsumerBaseV2, ERC721URIStorage, Ownable {
         uint32 callbackGasLimit,
         string[3] memory dogTokenUries,
         uint256 mintFee
-    ) VRFConsumerBaseV2(vrfCoordinatorV2) ERC721("Random IPFS NFT", "RAN") {
-        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+    )
+        VRFConsumerBaseV2(vrfCoordinatorV2)
+        ERC721("Random IPFS NFT", "RAN")
+    {
+        i_vrfCoordinator = VRFCoordinatorV2Interface(
+            vrfCoordinatorV2
+        );
         i_subscriptionId = subscriptionId;
         i_gasLane = gasLane;
         i_callbackGasLimit = callbackGasLimit;
@@ -53,74 +73,110 @@ contract RandomIpfsNft is VRFConsumerBaseV2, ERC721URIStorage, Ownable {
         i_mintFee = mintFee;
     }
 
-    function requestNft() public payable returns (uint256 requestId) {
+    function requestNft()
+        public
+        payable
+        returns (uint256 requestId)
+    {
         if (msg.value < i_mintFee) {
             revert RandomIpfsNft__NeedMoreETHSent();
         }
-        requestId = i_vrfCoordinator.requestRandomWords(
-            i_gasLane,
-            i_subscriptionId,
-            REQUEST_CONFIRMATIONS,
-            i_callbackGasLimit,
-            NUM_WORDS
-        );
-        s_requestIdSenders[requestId] = msg.sender;
+        requestId = i_vrfCoordinator
+            .requestRandomWords(
+                i_gasLane,
+                i_subscriptionId,
+                REQUEST_CONFIRMATIONS,
+                i_callbackGasLimit,
+                NUM_WORDS
+            );
+        s_requestIdSenders[requestId] = msg
+            .sender;
         emit NftRequested(requestId, msg.sender);
     }
 
-    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
-        internal
-        override
-    {
-        address requestSender = s_requestIdSenders[requestId];
-        uint256 moddedRng = randomWords[0] % MAX_CHANCE_VALUE;
-        Breed dogBreed = getBreedFromModdedRng(moddedRng);
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {
+        address requestSender = s_requestIdSenders[
+                requestId
+            ];
+        uint256 moddedRng = randomWords[0] %
+            MAX_CHANCE_VALUE;
+        Breed dogBreed = getBreedFromModdedRng(
+            moddedRng
+        );
         _safeMint(requestSender, s_tokenCounter);
-        _setTokenURI(s_tokenCounter, i_nftTokenUries[uint8(dogBreed)]);
+        _setTokenURI(
+            s_tokenCounter,
+            i_nftTokenUries[uint8(dogBreed)]
+        );
         s_tokenCounter = s_tokenCounter + 1;
         emit NftMinted(dogBreed, requestSender);
     }
 
     function withdraw() public onlyOwner {
         uint256 amount = address(this).balance;
-        (bool success, ) = payable(msg.sender).call{value: amount}("");
+        (bool success, ) = payable(msg.sender)
+            .call{value: amount}("");
         if (!success) {
             revert RandomIpfsNft__TransferFailed();
         }
     }
 
-    function getBreedFromModdedRng(uint256 moddedRng)
-        public
-        pure
-        returns (Breed)
-    {
+    function getBreedFromModdedRng(
+        uint256 moddedRng
+    ) public pure returns (Breed) {
         uint256 cumulativeSum = 0;
-        uint256[3] memory chanceArray = getChanceArray();
-        for (uint256 i = 0; i < chanceArray.length; i++) {
+        uint256[3]
+            memory chanceArray = getChanceArray();
+        for (
+            uint256 i = 0;
+            i < chanceArray.length;
+            i++
+        ) {
             if (
                 moddedRng >= cumulativeSum &&
-                moddedRng < cumulativeSum + chanceArray[i]
+                moddedRng < chanceArray[i]
             ) {
                 return Breed(i);
             }
-            cumulativeSum = cumulativeSum + chanceArray[i];
+            cumulativeSum =
+                cumulativeSum +
+                chanceArray[i];
         }
         revert RandomIpfsNft__RangeOutOfBounds();
     }
 
-    function getChanceArray() public pure returns (uint256[3] memory) {
-        return [10, 30, MAX_CHANCE_VALUE];
+    function getChanceArray()
+        public
+        pure
+        returns (uint256[3] memory)
+    {
+        return [10, 40, MAX_CHANCE_VALUE];
     }
 
-    function getMintFee() public view returns (uint256) {
+    function getMintFee()
+        public
+        view
+        returns (uint256)
+    {
         return i_mintFee;
     }
 
-    function getTokenCounter() public view returns (uint256) {
+    function getTokenCounter()
+        public
+        view
+        returns (uint256)
+    {
         return s_tokenCounter;
     }
 
-    function getTokenUries(uint256 index) public view returns (string memory) {
+    function getTokenUries(uint256 index)
+        public
+        view
+        returns (string memory)
+    {
         return i_nftTokenUries[index];
     }
 }
