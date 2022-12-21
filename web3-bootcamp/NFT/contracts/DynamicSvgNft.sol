5bbc30129bf14489ba4317141f464cc44baa003a
unknown <martin.petrov033@gmail.com>
2022-12-21 12:04:38 +0200

    Finish DynamicSvgNft smart contract implementation

--- web3-bootcamp/NFT/contracts/DynamicSvgNft.sol
+++ web3-bootcamp/NFT/contracts/DynamicSvgNft.sol
@@ -2,38 +2,48 @@
 pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "base64-sol/base64.sol";
 
+error ERC721Metadata_URI_QueryFor_NonExistentToken();
+error Stale_Data();
+
 contract DynamicSvgNft is ERC721 {
+    uint256 constant stalePriceDelay = 10 days;
     uint256 private s_tokenCounter;
     string private s_lowImageURI;
     string private s_highImageURI;
 
-    mapping(uint256 => uint256) private s_tokenIdHighValues;
+    AggregatorV3Interface internal immutable i_priceFeed;
+    mapping(uint256 => int256) private s_tokenIdHighValues;
 
-    event NftCreated(uint256 indexed tokenCounter, uint256 highValue);
+    event NftCreated(uint256 indexed tokenCounter, int256 highValue);
 
-    constructor()
-        // address priceFeedAddress,
-        // string memory lowSvg,
-        // string memory highSvg
-        ERC721("Dynamic SVG NFT", "DSN")
+    constructor(
+        address priceFeedAddress,
+        string memory lowSvg,
+        string memory highSvg
+    ) ERC721("Dynamic SVG NFT", "DSN") {
+        i_priceFeed = AggregatorV3Interface(priceFeedAddress);
+        s_lowImageURI = svgToImageURI(lowSvg);
+        s_highImageURI = svgToImageURI(highSvg);
+    }
+
+    function svgToImageURI(string memory svg)
+        public
+        pure
+        returns (string memory)
     {
-        s_lowImageURI = svgToImageURI();
-        s_highImageURI = svgToImageURI();
-    }
-
-    function svgToImageURI() public pure returns (string memory) {
         string memory svgBase64Encoded = Base64.encode(
-            bytes(string(abi.encodePacked()))
+            bytes(abi.encodePacked(svg))
         );
-        return svgBase64Encoded;
+        return string(abi.encodePacked(_baseURI(), svgBase64Encoded));
     }
 
-    function mintNft(uint256 highValue) public {
+    function mintNft(int256 highValue) public {
         s_tokenIdHighValues[s_tokenCounter] = highValue;
         s_tokenCounter = s_tokenCounter + 1;
-        _safeMint(msg.sender, highValue);
+        _safeMint(msg.sender, s_tokenCounter);
         emit NftCreated(s_tokenCounter, highValue);
     }
 
@@ -45,6 +55,42 @@ contract DynamicSvgNft is ERC721 {
         returns (string memory)
     {
         return "data:image/svg+xml;base64";
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        if (!_exists(tokenId)) {
+            revert ERC721Metadata_URI_QueryFor_NonExistentToken();
+        }
+
+        (, int256 price, uint256 updatedAt, , ) = i_priceFeed
+            .latestRoundData();
+
+        if (block.timestamp <= updatedAt + stalePriceDelay) {
+            revert Stale_Data();
+        }
+
+        string memory imageURI = s_lowImageURI;
+        if (price >= s_tokenIdHighValues[tokenId]) {
+            imageURI = s_highImageURI;
+        }
+
+        return
+            string(
+                abi.encodePacked(
+                    '{"name":"',
+                    name(),
+                    '", "description":"An NFT that changes based on the Chainlink Feed", ',
+                    '"attributes": [{"trait_type": "coolness", "value": 100}], "image":"',
+                    imageURI,
+                    '"}'
+                )
+            );
     }
 
     function getLowSvg() public view returns (string memory) {
