c450059fa52ce94bfea14e50d25a4e17cfffb14f
unknown <martin.petrov033@gmail.com>
2023-01-03 21:42:29 +0200

    Add Marketplace base setup

---
+++ web3-bootcamp/Marketplace/hardhat.config.js
@@ -0,0 +1,43 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+require("solidity-coverage");
+// require("hardhat-gas-reporter");
+require("hardhat-contract-sizer");
+require("dotenv").config();
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+        },
+    },
+    solidity: "0.8.17",
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+    etherscan: {
+        apiKey: {
+            goerli: ETHERSCAN_API_KEY,
+        },
+    },
+    mocha: {
+        timeout: 200000, // 200 seconds
+    },
+};
