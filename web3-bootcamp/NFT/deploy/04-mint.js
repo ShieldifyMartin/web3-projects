0c67c5442297c08bfe682bfa67a76f0065641bb5
unknown <martin.petrov033@gmail.com>
2023-01-04 21:43:16 +0200

    Finish nft deploy scripts

--- web3-bootcamp/NFT/deploy/04-mint.js
+++ web3-bootcamp/NFT/deploy/04-mint.js
@@ -13,23 +13,23 @@ module.exports = async function ({ getNamedAccounts }) {
 
     // Random IPFS NFT
     const randomIpfsNft = await ethers.getContract("RandomIpfsNft", deployer);
-    // const mintFee = await randomIpfsNft.getMintFee();
-    // const randomIpfsNftMintTx = await randomIpfsNft.requestNft({ value: mintFee.toString() });
-    // const randomIpfsNftMintTxReceipt = await randomIpfsNftMintTx.wait(1);
+    const mintFee = await randomIpfsNft.getMintFee();
+    const randomIpfsNftMintTx = await randomIpfsNft.requestNft({ value: mintFee.toString() });
+    const randomIpfsNftMintTxReceipt = await randomIpfsNftMintTx.wait(1);
 
-    // await new Promise(async (resolve, reject) => {
-    //     setTimeout(() => reject("Timeout: 'NftMinted' event did not fire"), 300000); // 5 minute timeout time
-    //     randomIpfsNft.once("NftMinted", async () => {
-    //         resolve();
-    //     })
-    //     if (chainId == 31337) {
-    //         const requestId = randomIpfsNftMintTxReceipt.events[1].args.requestId.toString();
-    //         const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer);
-    //         await vrfCoordinatorV2Mock.fulfillRandomWords(requestId, randomIpfsNft.address);
-    //     }
-    // });
-    // const randomIpfsNftTokenCounter = await dynamicNft.getTokenCounter();
-    // console.log(`Random IPFS NFT index 0 tokenURI: ${await randomIpfsNft.tokenURI(0)}`);
+    await new Promise(async (resolve, reject) => {
+        setTimeout(() => reject("Timeout: 'NftMinted' event did not fire"), 300000); // 5 minute timeout time
+        randomIpfsNft.once("NftMinted", async () => {
+            resolve();
+        })
+        if (chainId == 31337) {
+            const requestId = randomIpfsNftMintTxReceipt.events[1].args.requestId.toString();
+            const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer);
+            await vrfCoordinatorV2Mock.fulfillRandomWords(requestId, randomIpfsNft.address);
+        }
+    });
+    const randomIpfsNftTokenCounter = await dynamicNft.getTokenCounter();
+    console.log(`Random IPFS NFT index 0 tokenURI: ${await randomIpfsNft.tokenURI(0)}`);
 
     // Dynamic SVG NFT
     const highValue = ethers.utils.parseEther("4000");
