159b0ee0d84b1a86d4a9594eb7abda899aa09020
unknown <martin.petrov033@gmail.com>
2023-02-26 16:44:39 +0200

    Add DAO smart contracts

---
+++ web3-bootcamp/DAO/contracts/governance_standard/TimeLock.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract TimeLock is TimelockController {
+    constructor(
+        uint256 minDelay,
+        address[] memory proposers,
+        address[] memory executors
+    ) TimelockController(minDelay, proposers, executors, msg.sender) {}
+}
